abe664faff50eb7db50711aee7721f4834146508
joker <trakeolac@hotmail.com>
2016-10-03 09:56:05 +0700

    -- Add project

---
+++ src/Lottery.Entity/BaseEntity.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lottery.Entity
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+        }
+    }
+}
